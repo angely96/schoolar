d56d673f1d5b7e7922083cd5685ee08fb3bf459b
Aracelly <angelyaracelly321@gmail.com>
2025-04-03 09:56:07 -0500

    Encriptacion y verificacion del correo del usuario si esta repetido

--- src/signup.php
+++ src/signup.php
@@ -8,20 +8,40 @@ include ('../config/database.php');
  $email = $_POST['e_mail'];
  $passwd= $_POST['passw'];
 
- $sql ="INSERT INTO users( firstname, lastname, email,password)
-    values('$fname','$lname','$email','$passwd')
- 
- 
- ";
+ //$enc_pass= md5($passwd);
+//SHA 256 
+$enc_pass =sha1($passwd);
 
- $res= pg_query($conn,$sql);
+//Aqui voy a validad si el email ya existe en la bd
+$sql_email_exist = "
+   SELECT 
+      COUNT(email) as total  
+   FROM 
+      user 
+   WHERE  
+      email ='$email' 
+      LIMIT 1 ";
 
- if($res){
-    echo"User has been created succesfully";
- }else {
-    echo"Error ";
- }
+$res= pg_query($conn, $sql_email_exist);
 
+if($res){
+   $row = pg_fetch_assoc($res);
+   if($row['total']>0){
+      echo "Email already exist";
+   }
+   else{
+      $sql ="INSERT INTO users( firstname, lastname, email, password)
+         values('$fname','$lname','$email','$enc_pass')
+   ";
 
+      $res= pg_query($conn,$sql);
+
+      if($res){
+         echo"User has been created succesfully";
+         }else {
+            echo"Error ";
+         }
+      }
+   }
 
 ?>
