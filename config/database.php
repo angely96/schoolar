a74d3ef22993c22d165039ac235670a8e03fce6a
Aracelly <angelyaracelly321@gmail.com>
2025-04-04 18:04:43 -0500

    Data_base in Supabase

--- config/database.php
+++ config/database.php
@@ -3,13 +3,23 @@
 //Config connection 
 // lo que necesitamos para conectarnos 
 //--------------------------------------------
+/*
     $host = "localhost"; //Host, luego se cambia por la ip publica 
     $port="5432"; // puerto, en este caso de posgress
     $dbname="schoolar"; // nombre de la base de datos 
     $user="postgres"; // usuario
     $password ="postgres"; //contraseña, luego usamos variables de entorno
     //con es quien tiene la llave de toda la casa, tiene todos los privilegios. gi
+    */
+
     
+    $host = "aws-0-us-east-1.pooler.supabase.com"; 
+    $port="6543"; 
+    $dbname="postgres";  
+    $user="postgres.ojnwvpnaqszlwbmqdxjw"; 
+    $password ="unicesmag@@"; 
+    
+
 
 // Create connetion 
 $conn = pg_connect(" 
